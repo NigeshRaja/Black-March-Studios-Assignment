97f4692b89fdb64e1beae7e622c9d310fd3c2ae8
NigeshRaja <138748578+NigeshRaja@users.noreply.github.com>
2025-08-11 12:43:02 +0530

    n

--- Assets/Scripts/Cubes/CubeDetails.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class CubeDetails : MonoBehaviour
-{
-    public int row { get; set; }
-    public int col { get; set; }
-}
